ace7ee266b0522e649ec9bedcf4918f361316962
Pepijn Friederichs <pepijn@chello.nl>
2020-02-27 19:06:50 +0100

    bugfix

--- kenteken-check-nl.js
+++ kenteken-check-nl.js
@@ -1,4 +1,4 @@
-/* Ik heb deze kentekenCheck gebaseerd op de actuele formats(alle afgegeven kentekencombinaties) uitgegeven vanuit de RDW lijst welke is te vinden op bijgevoegde link. Voor een project had ik deze nodig en wilde het zelf even uitzoeken. Het doel was om het vooral simpel te houden. 
+/* Ik heb deze kentekenCheck gebaseerd op de actuele formats(alle afgegeven kentekencombinaties) uitgegeven vanuit de RDW lijst welke is te vinden op bijgevoegde link. Voor een project had ik deze nodig en wilde het zelf even uitzoeken. Het doel was om het vooral simpel te houden. De oplossing kan ook ingezet worden als HTML5 validation only in het 'pattern' attribuut, zie html.
 De open data API vd rdw retourneert geen koppeltekens in het kenteken voor zover ik weet, dus vandaar deze oplossing.
 De array van regex patronen correspondeert exact met de lijst van formats op de site vd RDW in bijgaande link.
 De functie 'kentekenCheck' kijkt of het een valide NL kenteken is, er worden geen klinkers gebruikt en geen tekens die de RDW voorschrijft. Kentekens met AA en CD zijn in deze functie niet meegenomen, de letters C en Q mogen niet meer vd overheid ivm interpretatie problemen en zijn wel meegenomen.
@@ -6,6 +6,8 @@ De functie 'kentekenCheck' kijkt of het een valide NL kenteken is, er worden gee
 De functie kan nog wat geoptimaliseerd worden door Array.find of Array.some, maar dan is een polyfill nodig  een for loop met een break is ook een oplossing.
 
 https://www.rdw.nl/particulier/voertuigen/auto/de-kentekenplaat/het-kenteken-op-de-plaat/uitleg-over-de-cijfers-en-letters-op-de-kentekenplaat
+
+ note: 'HTML5 input patterns don't accept flags so to get lowercase letters we need to add the uppercase and lowercase range (e.g. A-Za-z).'
 
 */
 
@@ -16,9 +18,10 @@ const kentekenCheck = (kenteken) => {
   
   if (typeof kenteken !== 'string') return;
   
-  const str = kenteken.toUpperCase();
+  let str = kenteken.toUpperCase();
   let newStr = '';
-  const arr = ['^([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})([0-9]{2})$',
+  // based on rdw demands
+  const arrRegEx = ['^([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})([0-9]{2})$',
                '^([0-9]{2})([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{2})$',
                '^([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})$',
                '^([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{2})$',
@@ -30,9 +33,12 @@ const kentekenCheck = (kenteken) => {
                '^([BDFGHJKLMNPRSTVWXYZ]{1})([0-9]{3})([BDFGHJKLMNPRSTVWXYZ]{2})$',
                '^([BDFGHJKLMNPRSTVWXYZ]{3})([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{1})$'];
 
-  kenteken = kenteken.trim().split('-').join(''); // trim whitespace / strip dashes
+  str = str.trim()
+           .split('-')
+           .join(''); // trim whitespace / strip dashes
+
   // returns true immediately when found match : legacy browser proof
-    let matchLicense = arr.some(regEx => {
+    let matchLicense = arrRegEx.some(regEx => {
 
       const re = new RegExp(regEx);
       const res = re.test(str);
@@ -46,10 +52,8 @@ const kentekenCheck = (kenteken) => {
   
   if (matchLicense){
     return newStr;
-  } else {
-    return newStr = 'Ongeldig kenteken'
-  }
-    
+  } 
+  return 'XX-XX-XX';
 }
 
 // vervang het voorbeeld met een geldig kenteken zonder/met verkeerd geplaatste koppeltekens
@@ -58,12 +62,13 @@ const kentekenCheck = (kenteken) => {
 //const result =  kentekenCheck('G244TR');
 //el.innerHTML = result;
 
-
-// om performance wat te verbeteren is het change event gebruikt
-document.getElementById('input-kenteken').addEventListener('change', (e) => {
-  el.innerHTML = kentekenCheck(e.target.value);
+// om met performance rekening te houden kan wellicht het change event worden gebruikt
+document.getElementById('input-kenteken').addEventListener('input', (e) => {
   
-});
+  let val = e.target.value;
+  el.innerHTML = kentekenCheck(val);
+  
+ });
 
 
 
