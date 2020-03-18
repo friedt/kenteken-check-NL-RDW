9bd1df80ac1d4773009d2e4ddae95aa949fdfcd5
Pepijn Friederichs <pepijn@chello.nl>
2020-03-18 15:56:48 +0100

    update with check on illegal combinations like SS and NSB

--- kenteken-check-nl.js
+++ kenteken-check-nl.js
@@ -2,20 +2,22 @@
 De open data API vd rdw retourneert geen koppeltekens in het kenteken voor zover ik weet, dus vandaar deze oplossing.
 De array van regex patronen correspondeert exact met de lijst van formats op de site vd RDW in bijgaande link.
 De functie 'kentekenCheck' kijkt of het een valide NL kenteken is, er worden geen klinkers gebruikt en geen tekens die de RDW voorschrijft. Kentekens met AA en CD zijn in deze functie niet meegenomen, de letters C en Q mogen niet meer vd overheid ivm interpretatie problemen en zijn wel meegenomen.
+Verboden combinaties: GVD, KKK, KVT, LPF, NSB, PKK, PSV, TBS, SS en SD (ook niet in lettercombinaties met 3 letters)
 
 De functie kan nog wat geoptimaliseerd worden door Array.find, maar dan is een polyfill nodig, een 'for' loop met een break is ook een oplossing.
 
 https://www.rdw.nl/particulier/voertuigen/auto/de-kentekenplaat/het-kenteken-op-de-plaat/uitleg-over-de-cijfers-en-letters-op-de-kentekenplaat
 
  note: 'HTML5 input patterns don't accept flags so to get lowercase letters we need to add the uppercase and lowercase range (e.g. A-Za-z).'
-
+ 
 MIT License
-
 Copyright (c) 2020 Pepijn Friederichs
+
 */
 
 const el = document.getElementById('kenteken');
 const inputEl = document.getElementById('input-kenteken');
+
 // start function kentekenCheck
 const kentekenCheck = (kenteken) => {
   
@@ -35,6 +37,8 @@ const kentekenCheck = (kenteken) => {
                '^([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{3})([BDFGHJKLMNPRSTVWXYZ]{1})$',
                '^([BDFGHJKLMNPRSTVWXYZ]{1})([0-9]{3})([BDFGHJKLMNPRSTVWXYZ]{2})$',
                '^([BDFGHJKLMNPRSTVWXYZ]{3})([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{1})$'];
+  
+  const forbiddenCharacters = /^((?!GVD|KKK|KVT|LPF|NSB|PKK|PSV|TBS|SS|SD).){6}$/;
 
   str = str.trim()
            .split('-')
@@ -45,9 +49,10 @@ const kentekenCheck = (kenteken) => {
 
       const re = new RegExp(regEx);
       const res = re.test(str);
+      const resLegal = forbiddenCharacters.test(str);
 
       // match on regex pattern 
-      if (res === true){
+      if (res === true && resLegal === true){
         inputEl.value = str.replace(re, '$1-$2-$3');        
         newStr = str.replace(re, '$1-$2-$3');
         return true;
