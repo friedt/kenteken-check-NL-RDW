ac67486874c96b9adb35bed0e242c088803d8f76
Pepijn Friederichs <pepijn@chello.nl>
2020-04-12 14:07:06 +0200

    change regex test early series 1,2, 3, vowels allowed

--- kenteken-check-nl-class.spec.js
+++ kenteken-check-nl-class.spec.js
@@ -1,14 +1,13 @@
-import {KentekenCheck} from "./kenteken-check-nl-class";
+import {KentekenCheck} from "./kenteken-check-class.js";
 
 
-describe('test kenteken-check-nl-class.js', function () {
+describe('kenteken-check-class.js', function () {
 
     let arrRegEx;
 
     beforeAll( function(){
-
-       let kt = new KentekenCheck();
-       arrRegEx = kt.arrRegEx;
+        let kt = new KentekenCheck();
+        arrRegEx = kt.arrRegEx;
     });
 
     it('getter method "license" should return string', function () {
@@ -21,11 +20,10 @@ describe('test kenteken-check-nl-class.js', function () {
 
     it('getter method "license" should return valid string', function () {
         // order of arr is important while corresponding index arrRegEx array
-        const arr = ['GF5643', '8765GG', '56TT54', 'GF88YY', 'HFFF43', '12HHGG', '12PTT8', '1KHH39', 'HG123R', 'G123TT', 'TRF12P'];
+        const arr = ['AF5643', '8765GE', '56TU54', 'GF88YZ', 'HFFF43', '12HHGG', '12PTT8', '1KHH39', 'HG123R', 'G123TT', 'TRF12P'];
         arr.forEach((item, index) => {
 
             const re = new RegExp(arrRegEx[index]);
-           
             const newStr = item.replace(re, '$1-$2-$3');
             let kt2 = new KentekenCheck(item);
             kt2.license;
@@ -38,18 +36,16 @@ describe('test kenteken-check-nl-class.js', function () {
     it('getter method "license" should return undefined when not string', function () {
         let kt2 = new KentekenCheck(987);
         kt2.license;
-
-        
+       
         expect(kt2.license).toBeUndefined();
 
     });
 
     it('getter method "license" should return a invalid sign XX-XX-XX', function () {
-        const arr = ['VVD56T', '12359T', 'SD6677', 'GHFRP5', 'UF4534', '987JK9', 'U123TT', 'PVV23R', 'QG6544'];
+        const arr = ['VVD56T', '12359T', 'SD6677', 'GHFRP5', 'HUFF34', '987JK9', 'U123TT', 'PVV23R', 'SSHH77', 'A675PP'];
         arr.forEach(item => {
             let kt2 = new KentekenCheck(item);
             kt2.license;
-
 
             expect(kt2.license).toEqual('XX-XX-XX');
         });
