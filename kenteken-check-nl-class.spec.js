43a176d3f375db259461542a108d44ed51fe5c41
Pepijn Friederichs <pepijn@chello.nl>
2020-04-16 12:58:07 +0200

    moved the input binding to the class

--- kenteken-check-nl-class.spec.js
+++ kenteken-check-nl-class.spec.js
@@ -1,53 +1,112 @@
-import {KentekenCheck} from "./kenteken-check-class.js";
+import {KentekenCheck} from "./kenteken-check-class";
+
+//jest.mock('./kenteken-check-class');
 
 
 describe('kenteken-check-class.js', function () {
 
     let arrRegEx;
+    let inputElm;
+    let outputElm;
+
+
 
     beforeAll( function(){
+
+        inputElm = document.createElement('input');
+        outputElm = document.createElement('div');
+        document.body.insertAdjacentHTML('afterbegin', inputElm);
+        document.body.insertAdjacentHTML('afterbegin', outputElm);
+
         let kt = new KentekenCheck();
         arrRegEx = kt.arrRegEx;
-    });
-
-    it('getter method "license" should return string', function () {
-        let kt2 = new KentekenCheck('GFYY54');
-        kt2.license;
-        
-        expect(kt2.license).toEqual('GF-YY-54');
 
     });
 
-    it('getter method "license" should return valid string', function () {
+    it('method "license" should return string', function () {
+        let kt2 = new KentekenCheck('GFYY54', inputElm);
+        inputElm.value = 'GFYY54';
+        const e = {
+            'target': {
+                value : 'GFYY54'
+            }
+        }
+        kt2.bindInputListener();
+        kt2.getValue(e);
+
+
+        //jest.spyOn(kt2, 'license', 'get').mockReturnValue('GFYY54');
+        expect(kt2.license()).toEqual('GF-YY-54');
+
+    });
+
+    it('method "license" should return valid string', function () {
         // order of arr is important while corresponding index arrRegEx array
-        const arr = ['AF5643', '8765GE', '56TU54', 'GF88YZ', 'HFFF43', '12HHGG', '12PTT8', '1KHH39', 'HG123R', 'G123TT', 'TRF12P'];
+        const arr = ['GF5643', '8765GG', '56TT54', 'GF88YY', 'HFFF43', '12HHGG', '12PTT8', '1KHH39', 'HG123R', 'G123TT', 'TRF12P'];
         arr.forEach((item, index) => {
+            let kt2 = new KentekenCheck(item, inputElm);
+
+            const e = {
+                'target': {
+                    value : item
+                }
+            }
+            kt2.bindInputListener();
+            kt2.getValue(e);
 
             const re = new RegExp(arrRegEx[index]);
+            //jest.spyOn(kt2, 'license', 'get').mockReturnValue('GFYY54');
             const newStr = item.replace(re, '$1-$2-$3');
-            let kt2 = new KentekenCheck(item);
-            kt2.license;
 
-            expect(kt2.license).toEqual(newStr);
+
+            //kt2.license;
+
+            expect(kt2.license()).toEqual(newStr);
         });
 
     });
 
-    it('getter method "license" should return undefined when not string', function () {
-        let kt2 = new KentekenCheck(987);
-        kt2.license;
-       
-        expect(kt2.license).toBeUndefined();
+    it('method "license" should return undefined when not string', function () {
+        let kt2 = new KentekenCheck(987, inputElm);
+
+
+        //jest.spyOn(kt2, 'license', 'get').mockReturnValue('GFYY54');
+        expect(kt2.license()).toBeUndefined();
 
     });
 
-    it('getter method "license" should return a invalid sign XX-XX-XX', function () {
-        const arr = ['VVD56T', '12359T', 'SD6677', 'GHFRP5', 'HUFF34', '987JK9', 'U123TT', 'PVV23R', 'SSHH77', 'A675PP'];
-        arr.forEach(item => {
-            let kt2 = new KentekenCheck(item);
-            kt2.license;
+    it('method "showInContainer" should display license in the container', function () {
+        let kt2 = new KentekenCheck('GFYY54', inputElm, outputElm, true);
+        const e = {
+            'target': {
+                value : 'GFYY54'
+            }
+        }
+        kt2.bindInputListener();
+        kt2.getValue(e);
 
-            expect(kt2.license).toEqual('XX-XX-XX');
+
+        //jest.spyOn(kt2, 'license', 'get').mockReturnValue('GFYY54');
+        expect(kt2.output).toEqual(true);
+
+    });
+
+    it('method "license" should return a invalid sign XX-XX-XX', function () {
+        const arr = ['VVD56T', '12359T', 'SD6677', 'GHFRP5', 'UF4534', '987JK9', 'U123TT', 'PVV23R', 'QG6544'];
+        arr.forEach(item => {
+            let kt2 = new KentekenCheck(item, inputElm);
+
+            const e = {
+                'target': {
+                    value : item
+                }
+            }
+            kt2.bindInputListener();
+            kt2.getValue(e);
+
+            //jest.spyOn(kt2, 'license', 'get').mockReturnValue('GFYY54');
+
+            expect(kt2.license()).toEqual('XX-XX-XX');
         });
 
 
