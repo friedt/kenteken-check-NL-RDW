75310c136e1a7738cdef0ecd30a71f4703f0b363
Pepijn Friederichs <pepijn@chello.nl>
2020-04-18 15:30:21 +0200

    format

--- src/kenteken-check-nl-class.js
+++ src/kenteken-check-nl-class.js
@@ -25,7 +25,7 @@ Copyright (c) 2020 Pepijn Friederichs
 // start class KentekenCheck
 export class KentekenCheck {
 
-    constructor(kenteken, inputElm, outputElm, output = false, classValid  = 'valid'){
+    constructor(kenteken, inputElm, outputElm, output = false, classValid = 'valid') {
         this.newStr = '';
         this.output = output;
         this.kenteken = kenteken;
@@ -33,21 +33,21 @@ export class KentekenCheck {
         this.outputElm = outputElm;
         this.classValid = classValid;
         this.arrRegEx = ['^([A-Z]{2})([0-9]{2})([0-9]{2})$', // XX9999
-               '^([0-9]{2})([0-9]{2})([A-Z]{2})$', // 9999XX
-               '^([0-9]{2})([A-Z]{2})([0-9]{2})$', // 99XX99
-               '^([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{2})$',// XX99XX
-               '^([BDFGHJKLMNPRSTVWXYZ]{2})([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})$',// XXXX99
-               '^([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{2})([BDFGHJKLMNPRSTVWXYZ]{2})$',// 99XXXX
-               '^([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{3})([0-9]{1})$',// 99XXX9
-               '^([0-9]{1})([BDFGHJKLMNPRSTVWXYZ]{3})([0-9]{2})$',// 9XXX99
-               '^([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{3})([BDFGHJKLMNPRSTVWXYZ]{1})$',// XX999X
-               '^([BDFGHJKLMNPRSTVWXYZ]{1})([0-9]{3})([BDFGHJKLMNPRSTVWXYZ]{2})$',// X999XX
-               '^((?!PVV|VVD|SGP)[BDFGHJKLMNPRSTVWXYZ]{3})([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{1})$'];// XXX99X
+            '^([0-9]{2})([0-9]{2})([A-Z]{2})$', // 9999XX
+            '^([0-9]{2})([A-Z]{2})([0-9]{2})$', // 99XX99
+            '^([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{2})$',// XX99XX
+            '^([BDFGHJKLMNPRSTVWXYZ]{2})([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})$',// XXXX99
+            '^([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{2})([BDFGHJKLMNPRSTVWXYZ]{2})$',// 99XXXX
+            '^([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{3})([0-9]{1})$',// 99XXX9
+            '^([0-9]{1})([BDFGHJKLMNPRSTVWXYZ]{3})([0-9]{2})$',// 9XXX99
+            '^([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{3})([BDFGHJKLMNPRSTVWXYZ]{1})$',// XX999X
+            '^([BDFGHJKLMNPRSTVWXYZ]{1})([0-9]{3})([BDFGHJKLMNPRSTVWXYZ]{2})$',// X999XX
+            '^((?!PVV|VVD|SGP)[BDFGHJKLMNPRSTVWXYZ]{3})([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{1})$'];// XXX99X
 
         this.forbiddenCharacters = /^((?!GVD|KKK|KVT|LPF|NSB|PKK|PSV|TBS|SS|SD).){6}$/;
     }
 
-    formatLicense(){
+    formatLicense() {
         if (typeof this.kenteken !== 'string') return;
 
         const str = this.kenteken.toUpperCase()
@@ -65,7 +65,7 @@ export class KentekenCheck {
             const resLegal = this.forbiddenCharacters.test(str);
 
             // match on regex pattern
-            if (res === true && resLegal === true){
+            if (res === true && resLegal === true) {
                 this.inputElm.value = str.replace(re, '$1-$2-$3');
                 this.inputElm.classList.add(this.classValid);
                 this.newStr = str.replace(re, '$1-$2-$3');
@@ -75,13 +75,13 @@ export class KentekenCheck {
         });
     }
 
-    showLicense(str){
+    showLicense(str) {
 
         // based on rdw demands
         // returns true immediately when found match : legacy browser proof IE 9/10/11, no polyfill needed
         const matchLicense = this.matchLicense(str);
         //console.log('match', matchLicense);
-        if (matchLicense){
+        if (matchLicense) {
             this.showInContainer(this.newStr);
             return this.newStr;
         }
@@ -90,14 +90,14 @@ export class KentekenCheck {
         return 'XX-XX-XX';
     }
 
-    showInContainer(str){
-        if (this.output){
+    showInContainer(str) {
+        if (this.output) {
             this.outputElm.innerHTML = str;
         }
     }
 
-    getValue(e){
-        if (e.target.value.length >= 6){
+    getValue(e) {
+        if (e.target.value.length >= 6) {
             this.kenteken = e.target.value;
             this.formatLicense();
         }
