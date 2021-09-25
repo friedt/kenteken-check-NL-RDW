578abf1985f429121c1e496b23d94565d9e6d04c
Pepijn Friederichs <pepijn@chello.nl>
2021-09-25 04:32:28 +0200

    Empty lines

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import {KentekenCheck} from './kenteken-check-nl-class.js';
 
-
 const outputElm = document.getElementById('kenteken');
 const inputElm = document.getElementById('input-kenteken');
-
 
 const kt = new KentekenCheck('S007JB', inputElm, outputElm, true);
 kt.formatLicense();
