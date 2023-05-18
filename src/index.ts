1a8fe07f6860884559f83d1cc7e91bd6d522e2fe
Pepijn Friederichs <pepijn@chello.nl>
2023-05-18 08:41:44 +0200

    plain example added without styling

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import {KentekenCheck} from './kenteken-check-nl-class-ts';
 
 const outputElm = document.getElementById('kenteken') as HTMLDivElement;
 const inputElm = document.getElementById('input-kenteken') as HTMLInputElement;
-
+const label = document.createElement('label');
+label.setAttribute("style", 'margin-top: 1rem; border-top: 2px dashed')
+label.innerText = "Voer kenteken in";
+const containerInput = document.createElement('input');
+const containerOutput = document.createElement('div');
+document.body.appendChild(label);
+document.body.appendChild(containerInput);
+document.body.appendChild(containerOutput);
 
 const kt = new KentekenCheck('S007HH', inputElm, outputElm);
 kt.formatLicense();
@@ -11,3 +18,8 @@ kt.bindInputListener();
 // format only
 const kt2 = new KentekenCheck('JFK01P');
 outputElm.innerHTML = kt2.formatLicense() as string;
+
+//another example
+const kt3 = new KentekenCheck('hfff43', containerInput, containerOutput, 'valid', "Kenteken bestaat niet");
+kt3.formatLicense();
+kt3.bindInputListener();
