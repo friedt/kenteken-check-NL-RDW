3d278f69d38970572e3c25314ae7b26ff6337236
Pepijn Friederichs <pepijn@chello.nl>
2023-05-31 23:08:04 +0200

    add declaration files

---
+++ types/kenteken-check-nl-class-ts.d.ts
@@ -0,0 +1,20 @@
+export declare class KentekenCheck {
+    newStr: string;
+    index: number;
+    kenteken: string;
+    valid: boolean;
+    inputElm: HTMLInputElement | null;
+    outputElm: HTMLElement | null;
+    classValid: string;
+    arrRegEx: Array<string>;
+    forbiddenCharacters: RegExp;
+    errorMessage: string;
+    constructor(kenteken: string, inputElm?: HTMLInputElement, outputElm?: HTMLElement, classValid?: string, errorMessage?: string);
+    formatLicense(): string;
+    matchLicense(str: string): boolean;
+    checkForbiddenCharacters(str: string): boolean;
+    showLicense(str: string): string;
+    showInContainer(str: string): string;
+    getValue(e: Event): void;
+    bindInputListener(event?: string): void;
+}
